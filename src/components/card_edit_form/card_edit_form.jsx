1239b297e1c3c822842e9c10dc1975f9726e9471
devy688 <devyoung688@gmail.com>
2021-11-29 16:20:34 +0900

    Update card editor part

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -1,17 +1,9 @@
-import React from "react";
-import styles from "./card_edit_form.module.css";
+import React, { memo, useCallback } from "react";
 import Button from "../button/button";
+import styles from "./card_edit_form.module.css";
 
-const CardEditForm = ({ FileInput, card, updateCard, deleteCard }) => {
+const CardEditForm = memo(({ FileInput, card, updateCard, deleteCard }) => {
   const { name, company, title, email, message, theme, fileName } = card;
-
-  const onFileChange = (file) => {
-    updateCard({
-      ...card,
-      fileName: file.name,
-      fileURL: file.url,
-    });
-  };
 
   const onChange = (event) => {
     if (event.currentTarget === null) return;
@@ -26,12 +18,24 @@ const CardEditForm = ({ FileInput, card, updateCard, deleteCard }) => {
     deleteCard(card);
   };
 
+  const onFileChange = useCallback(
+    (file) => {
+      updateCard({
+        ...card,
+        fileName: file.name,
+        fileURL: file.url,
+      });
+    },
+    [card, updateCard]
+  );
+
   return (
     <form className={styles.form}>
       <input
         className={styles.input}
         type="text"
         name="name"
+        placeholder="Name"
         value={name}
         onChange={onChange}
       />
@@ -39,6 +43,7 @@ const CardEditForm = ({ FileInput, card, updateCard, deleteCard }) => {
         className={styles.input}
         type="text"
         name="company"
+        placeholder="Company"
         value={company}
         onChange={onChange}
       />
@@ -48,14 +53,18 @@ const CardEditForm = ({ FileInput, card, updateCard, deleteCard }) => {
         value={theme}
         onChange={onChange}
       >
-        <option value="light">light</option>
-        <option value="dark">dark</option>
-        <option value="colorful">colorful</option>
+        <option value="lightGrey">Light Grey</option>
+        <option value="grey">Grey</option>
+        <option value="yellow">Yellow</option>
+        <option value="pink">Pink</option>
+        <option value="skyblue">SkyBlue</option>
+        <option value="purple">Purple</option>
       </select>
       <input
         className={styles.input}
         type="text"
         name="title"
+        placeholder="Title"
         value={title}
         onChange={onChange}
       />
@@ -63,6 +72,7 @@ const CardEditForm = ({ FileInput, card, updateCard, deleteCard }) => {
         className={styles.input}
         type="text"
         name="email"
+        placeholder="Email"
         value={email}
         onChange={onChange}
       />
@@ -70,6 +80,7 @@ const CardEditForm = ({ FileInput, card, updateCard, deleteCard }) => {
         className={styles.textarea}
         value={message}
         name="message"
+        placeholder="Message"
         onChange={onChange}
       ></textarea>
       <div className={styles.fileInput}>
@@ -78,6 +89,6 @@ const CardEditForm = ({ FileInput, card, updateCard, deleteCard }) => {
       <Button name="Delete" onClick={onSubmit} />
     </form>
   );
-};
+});
 
 export default CardEditForm;
--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { memo } from "react";
 import CardAddForm from "../card_add_form/card_add_form";
 import CardEditForm from "../card_edit_form/card_edit_form";
 import styles from "./editor.module.css";
 
-const Editor = ({ FileInput, cards, addCard, updateCard, deleteCard }) => (
-  <section className={styles.editor}>
-    <h1 className={styles.title}>Card Maker</h1>
-    {Object.keys(cards).map((key) => (
-      <CardEditForm
-        key={key}
-        FileInput={FileInput}
-        card={cards[key]}
-        updateCard={updateCard}
-        deleteCard={deleteCard}
-      />
-    ))}
-    <CardAddForm FileInput={FileInput} onAdd={addCard} />
-  </section>
-);
+const Editor = memo(({ FileInput, cards, addCard, deleteCard, updateCard }) => {
+  return (
+    <section className={styles.editor}>
+      <h1 className={styles.title}>Card Maker</h1>
+      {Object.keys(cards).map((key) => {
+        return (
+          <CardEditForm
+            key={key}
+            FileInput={FileInput}
+            card={cards[key]}
+            updateCard={updateCard}
+            deleteCard={deleteCard}
+          />
+        );
+      })}
+      <CardAddForm FileInput={FileInput} addCard={addCard} />
+    </section>
+  );
+});
 
 export default Editor;
